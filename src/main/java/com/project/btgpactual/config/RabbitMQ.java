9b0a0fcb8e6938db984212a968b56f3a2a688248
Luiz Fernando <luizfernandosant26@gmail.com>
2025-12-02 22:08:32 -0300

    rabbit and mongodb configurations

---
+++ src/main/java/com/project/btgpactual/config/RabbitMQ.java
@@ -0,0 +1,10 @@
+package com.project.btgpactual.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQ {
+
+    public static final String ORDER_CREATED_QUEUE = "order-created-queue";
+    
+}
